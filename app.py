36c7c1fb0773f9a9bd43031d2dc64bda1f090b08
dclahan <dclahan@users.noreply.github.com>
2024-06-01 17:03:22 -0400

    init commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+import openai #dunno doesnt work rn i think
+import os
+from dotenv import load_dotenv
+
+#https://platform.openai.com/account/api-keys
+
+load_dotenv()
+
+openai.api_key = os.environ["OPENAI_API_KEY"]
+
+# print(openai.api_key)
+
+# Set up Flask app
+
+# 2 endpts one for chat and then one to put query into openai
+
+app = Flask(__name__)
+
+# Define home page route
+# user enters here
+
+@app.route("/")
+def home():
+    return render_template("index.html")
